87efcab65f70b912720748fc3d87b856ca113591
Alena-Belousova <107402810+Alena-Belousova@users.noreply.github.com>
2022-07-09 20:51:32 +0300

    заменена задача 38

---
+++ task_38.cs
@@ -0,0 +1,22 @@
+﻿// Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+Console.WriteLine("задача 38");
+Console.Write("введите размер массива: ");//5
+int size = Convert.ToInt32(Console.ReadLine());
+
+double[] array=new double[size];
+
+for(int index = 0; index<size; index++)
+{
+    array[index] =new Random().NextDouble()*100;
+    array[index]=Math.Round(array[index],2);
+    Console.Write(array[index] + " ");
+    Console.WriteLine();
+    }
+    double numberMax=array.Max();
+        Console.WriteLine("максимум "+numberMax );
+        double numberMin=array.Min();
+        Console.WriteLine("минимум "+numberMin );
+        double itog=numberMax- numberMin;
+       itog= Math.Round(itog,2);
+        Console.WriteLine("разница между максимальным и минимальным элементами = "+itog );
